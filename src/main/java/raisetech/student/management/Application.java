17a728fca0383092cddd3e912afe9cfbd0583ecf
fujisawa <yyy_ccc@yahoo.ne.jp>
2024-08-26 13:56:38 +0900

    準備

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,22 @@
+package raisetech.student.management;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+  @GetMapping("/hello")
+	public String hello() {
+		boolean isAllLowerCase = StringUtils.isAllLowerCase("abcd");
+		return "IS 'abcd' all lowercase? " + isAllLowerCase;
+	}
+}
